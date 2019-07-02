f12a364373a33b71d3c837e52ca333d133839d96
vinamraRathi <vinamrarathi771@gmail.com>
2019-07-02 14:59:32 +0530

    Reverted back to normal

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -39,7 +39,6 @@ class signup extends Component {
                     </div>
                     <div className="input-field">
                         <button className="btn pink lighten-1 z-depth-0">Login</button>
-                        <button>Konichiwa</button>
                     </div>
                 </form>
             </div>
